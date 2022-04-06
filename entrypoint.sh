eca53b122254286460c09cd5ae2e01c5779745b3
amiribenabdellah.personal <amiribenabdellah@gmail.com>
2022-04-06 10:25:34 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
-apt-get update && apt-get -y install zip unzip
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === file_get_contents('https://composer.github.io/installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-
-mv composer.phar /usr/local/bin/composer
-
-RUN ls -la /tests
-
 #!/bin/bash
 set -e
 phplint ./
