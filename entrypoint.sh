aa18d635d15d7921a43a247503ea1b2e9e80ceb2
amiribenabdellah.personal <amiribenabdellah@gmail.com>
2022-04-06 09:27:06 +0200

    test

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+apt-get update && apt-get -y install zip unzip
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === file_get_contents('https://composer.github.io/installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+mv composer.phar /usr/local/bin/composer
+
+mkdir /phplint && cd /phplint && composer require overtrue/phplint && ln -s /phplint/vendor/bin/phplint /usr/local/bin/phplint
+
+cp "." "/tests"
+
+RUN ls -la /tests
+
+#!/bin/bash
+set -e
+phplint ./
